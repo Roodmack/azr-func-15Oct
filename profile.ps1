dd330a7424c8a65c2928f296a2d7833a6ea271f0
Roodmack <lerevit@lerevit.onmicrosoft.com>
2024-10-17 03:13:36 +0200

    My fisrt commit rm change

--- profile.ps1
+++ profile.ps1
@@ -1,7 +1,7 @@
 # Azure Functions profile.ps1
 #
 # This profile.ps1 will get executed every "cold start" of your Function App.
-# "cold start" occurs when********:
+# "cold start" occurs when:
 #
 # * A Function App starts up for the very first time
 # * A Function App starts up after being de-allocated due to inactivity
